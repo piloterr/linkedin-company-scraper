551b03330cab66a84d22e8ba68330391f401405b
Josselin Liebe <josselin.liebe@gmail.com>
2024-02-13 20:16:50 +0100

    fix thread lock file json

--- linkedin-company-scraper-json/linkedin_company.py
+++ linkedin-company-scraper-json/linkedin_company.py
@@ -4,7 +4,7 @@ import csv
 import json
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from threading import Semaphore
+from threading import Semaphore, Lock
 from dotenv import load_dotenv
 from termcolor import colored
 from tqdm import tqdm
@@ -17,6 +17,9 @@ MAX_WORKERS = int(os.getenv("MAX_WORKERS", 5))
 
 # Semaphore to limit concurrent requests per second
 semaphore = Semaphore(MAX_WORKERS)
+
+# Thread lock to synchronize access to the JSON file
+json_file_lock = Lock()
 
 # Request to the API with rate limiting
 def get_company_info(slug):
@@ -31,25 +34,28 @@ def get_company_info(slug):
             update_json(slug, company_info)
             return company_info
         except requests.exceptions.HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")
+            # See 404, 500, etc. errors
+            # print(f"HTTP error occurred: {http_err}")
+            pass
         except requests.exceptions.RequestException as req_err:
             print(f"Request error occurred: {req_err}")
         finally:
             # Wait before releasing the semaphore to adhere to rate limit
             time.sleep(1 / 5)  # Wait 1/5th of a second before allowing a new request
 
-# Function to update the JSON file
+# Update the JSON file with the company information
 def update_json(slug, company_info):
-    try:
-        with open('results.json', 'r', encoding='utf-8') as json_file:
-            result_dict = json.load(json_file)
-    except FileNotFoundError:
-        result_dict = {}
-
-    result_dict[slug] = company_info
-
-    with open('results.json', 'w', encoding='utf-8') as json_file:
-        json.dump(result_dict, json_file, ensure_ascii=False, indent=2)
+    with json_file_lock:  # Use a lock to prevent concurrent access to the file
+        try:
+            with open('results.json', 'r', encoding='utf-8') as json_file:
+                result_dict = json.load(json_file)
+        except FileNotFoundError:
+            result_dict = {}
+    
+        result_dict[slug] = company_info
+    
+        with open('results.json', 'w', encoding='utf-8') as json_file:
+            json.dump(result_dict, json_file, ensure_ascii=False, indent=2)
 
 # Process slugs from a CSV file with progress bar
 def process_slugs(rows):
